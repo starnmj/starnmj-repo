979c1fb041d27761ec766608827171fa98250c79
Min Jong Noh <47522172+starnmj@users.noreply.github.com>
2019-09-18 13:52:55 +0900

    NanoCore with Python3

---
+++ env.py
@@ -0,0 +1,12 @@
+#
+# SIESTA
+#
+
+siesta_calculator = '~/mybin/siesta_class'
+siesta_psf_location = '/home2/jhiskard/mybin/psf'
+siesta_util_location = '~/mybin'
+siesta_util_band = 'new.gnubands'
+siesta_util_dos  = 'Eig2DOS'
+siesta_util_pdos = 'fmpdos'
+siesta_util_rho  = 'rho2xsf'
+siesta_util_vh   = 'macroave'
